16d50ddd5970e8ea76772e7a97cb999df72caf4a
Bori7 <oluwaboriife@gmail.com>
2020-08-24 13:52:47 +0100

    database works

--- home/settings/prod.py
+++ home/settings/prod.py
@@ -2,13 +2,13 @@
 
 from .base import *
 
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS += ['0.0.0.0','localhost','cheart2020.herokuapp.com','127.0.0.1', 'localhost'] 
 
 
 WSGI_APPLICATION = 'home.wsgi.prod.application'
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = os.environ.get('SECRET_KEY')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
@@ -16,10 +16,10 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dek6rn3qtancpu',
-        'USER': 'lxgewggjjrvyba',
-        'PASSWORD': 'd4741d7c9a2b44c2dc7b51b68e7c5d56be306c2f3f9ac43906c9c23a2978604b',
-        'HOST': 'ec2-34-236-215-156.compute-1.amazonaws.com',
+        'NAME': 'cheart',
+        'USER': 'postgres',
+        'PASSWORD': 'asdfghjklpoiuytrewq',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
@@ -50,6 +50,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #     'http://localhost:3000',
 #     'http://127.0.0.1:8000',
 #     'http://192.168.8.100:3000',
-)
+#)
 
 CORS_ORIGIN_ALLOW_ALL = True 
