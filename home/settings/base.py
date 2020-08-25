4eb224068d1d81b4f4a5a67e0eb89234097e4942
Bori7 <oluwaboriife@gmail.com>
2020-08-25 02:29:58 +0100

    making it better

--- home/settings/base.py
+++ home/settings/base.py
@@ -18,7 +18,8 @@ INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
     'django.contrib.sites',
     'allauth',
-    
+    'allauth.account',
+    'allauth.socialaccount',
     'corsheaders',
     'rest_auth',
     'rest_auth.registration',
