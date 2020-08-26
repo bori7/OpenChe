ad8c5d35474d9e2ab6285e21443737b0efb7bc5a
Bori7 <oluwaboriife@gmail.com>
2020-08-26 06:28:53 +0100

    Implement database connection

--- home/settings/prod.py
+++ home/settings/prod.py
@@ -6,7 +6,7 @@ from .base import *
 
 
 DEBUG = True
-ALLOWED_HOSTS += ['0.0.0.0','localhost','openche2020.herokuapp.com','127.0.0.1', 'localhost'] 
+ALLOWED_HOSTS += ['openche2020.herokuapp.com'] 
 
 
 WSGI_APPLICATION = 'home.wsgi.prod.application'
