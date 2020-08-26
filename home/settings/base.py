c6fa3f12d6277afb00d79886859da5938fffdffa
Bori7 <oluwaboriife@gmail.com>
2020-08-26 07:41:52 +0100

    Implement database connection

--- home/settings/base.py
+++ home/settings/base.py
@@ -14,7 +14,7 @@ if os.path.isfile(dotenv_file):
 
 SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
 DEBUG = True
-ALLOWED_HOSTS = [ 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [ 'localhost']
 
 
 
